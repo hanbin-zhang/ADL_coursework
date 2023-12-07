0feb607f8a621d03e48b4f64fa2a8adbf474ecff
Yichen Dong <yc_dong@outlook.com>
2023-12-07 14:18:50 +0000

    Update train.sh

--- train.sh
+++ train.sh
@@ -15,4 +15,8 @@ module purge
 # load in the module dependencies for this script
 module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
 # achieve 85.50 with adamW and lr=0.003  loss=0.15866
+
+# Please note, if you want run this script, please have a look at the README.md file
+# to determine the modifications on instructions. Here are just examples.
+
 python train.py --learning-rate 0.003 --sgd-momentum 0.9
